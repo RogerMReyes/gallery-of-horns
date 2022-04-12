1dcbfc1f2c32d11c0428317dc43cdf3a4bfaee45
Roger Reyes <rogerrmr14@gmail.com>
2022-04-11 17:13:43 -0700

    Created component for footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <h3>
+        Roger Reyes
+      </h3>
+    )
+  }
+}
+
+export default Footer;
